17d332e54b7f39d89e575bcb3de6f1aa66a69d4c
Jarmel <pawel.jarmula@gmail.com>
2023-04-26 20:38:55 +0200

    styles and userlist data collecting added

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card.js'
 import './AddUser.css'
+import Button from '../UI/Button.js'
+
+const usersArray = []
 
 const AddUser = () => {
+    
     const addUserHandler = event => {
         event.preventDefault()
+
+        if (enteredUsername!='' && enteredAge!='' && enteredAge>=1) {
+            console.log(enteredUsername, enteredAge)
+            setEnteredUsername('')
+            setEnteredAge('')
+        } else {
+            return;
+        }
+
+        const usernameInfo = {
+            username: enteredUsername,
+            age: enteredAge
+        }
+
+        usersArray.push(usernameInfo)
+        console.log(usersArray)
+    }
+
+    const [enteredUsername, setEnteredUsername] = useState('')
+    const [enteredAge, setEnteredAge] = useState('')
+
+    const usernameChangeHandler = event => {
+        setEnteredUsername(event.target.value)
+    }
+    const ageChangeHandler = event => {
+        setEnteredAge(event.target.value)
     }
 
     return (
         <Card>
             <form onSubmit={addUserHandler}>
                 <label htmlFor='username' >Username</label>
-                <input id='username' />
+                <input id='username' onChange={usernameChangeHandler} value={enteredUsername} />
                 <label htmlFor='age'>Age</label>
-                <input id='age' type='number' />
-                <button type='submit'>Add User</button>
+                <input id='age' type='number' onChange={ageChangeHandler} value={enteredAge} />
+                <Button type='submit'>Add User</Button>
             </form>
         </Card>
     )
